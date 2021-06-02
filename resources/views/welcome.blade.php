ccf2a7ba8ef1b31da3c867a983b2e15271e00cc2
marbelbel0 <marbelbel0@gmail.com>
2021-06-02 23:14:58 +0700

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Arif Heroku
                 </div>
 
                 <div class="links">
